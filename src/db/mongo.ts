042fb8df7466aa49be4795aa7132c0f2a46254ba
Yash Priyam <yash@adroitcc.io>
2025-10-07 13:23:50 +0530

    add db name

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -4,7 +4,9 @@ import { env } from '../config/env.js';
 export async function initMongo() {
   mongoose.set('strictQuery', true);
   console.log('Connecting to MongoDB...');
-  await mongoose.connect(env.MONGO_URI);
+  await mongoose.connect(env.MONGO_URI!, {
+    dbName: "vaani_dev",
+  });
   console.log('Connected to MongoDB');
 }
 
